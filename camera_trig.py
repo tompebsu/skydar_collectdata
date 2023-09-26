6807331bb8b1c3747aa297bf9a63e88cdc94d172
tom pe <35053430+tompebsu@users.noreply.github.com>
2023-09-25 21:54:08 -0600

    Add files via upload

---
+++ camera_trig.py
@@ -0,0 +1,33 @@
+#Triggering Camera 
+
+import gphoto2 as gp
+from time import sleep
+from datetime import datetime
+
+camera = gp.Camera()
+
+i = 0
+while(1):
+    #initiate camera
+    camera.init()
+    now = datetime.now()
+    
+    #timestamp 
+    date = now.strftime("%d")
+    month = now.strftime("%m")
+    year = now.strftime("%y")
+    hour = now.strftime("%H")
+    minute = now.strftime("%M")
+    second = now.strftime("%S")
+    
+    #file path 
+    file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
+    target = '/home/pi/camera_pics/capt'+ hour + minute +second + '_'+str(i) + '.jpg' 
+    camera_file = camera.file_get(file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+    
+    #save pic
+    camera_file.save(target)
+    camera.exit()
+    sleep(1)
+    i = i + 1
+
